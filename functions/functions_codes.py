16e3308247abca5cdcce2af3e169850abe469296
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-07-16 06:26:56 +0000

    functions

---
+++ functions/functions_codes.py
@@ -0,0 +1,32 @@
+def add(x,y):
+    z=x+y
+    return z
+    
+a=add(10,20)
+print(a)
+
+def mul(x,y):
+    z=x*y
+    print(z)
+    
+mul(10,20)
+
+def sub():
+    x=int(input("enter the num"))
+    y=int(input("enter the num"))
+    z=x-y
+    return z
+    
+s=sub()
+print(s)
+
+
+def div():
+    print("without arguments without return type")
+    x=1
+    y=2
+    z=x/y
+    print(z)
+    
+div()
+
