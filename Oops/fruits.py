b9017ca4e83bcc0aff0bbea5539594abe1f94792
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-07-29 11:11:20 +0000

    oops

---
+++ Oops/fruits.py
@@ -0,0 +1,43 @@
+# multilevel fruits class
+
+class fruits:
+    def __init__(self,name,color):
+        self.f_name=name
+        self.f_color=color
+        
+    def display(self):
+        print("the name of the fruit is :",self.f_name)
+        print("the color of the fruit is:",self.f_color)
+        
+        
+class fruits2(fruits):
+    def __init__(self,name,color,price):
+        super().__init__(name,color)
+        self.f_price=price
+        
+    def display(self):
+        super().display()
+        print("the price of the fruit is:",self.f_price)
+        
+        
+class fruits3(fruits2):
+    def __init__(self,name,color,price,availability):
+        super().__init__(name,color,price)
+        self.f_availability=availability
+        
+    def display(self):
+        super().display()
+        print("the availability of the fruits are:",self.f_availability)
+
+        
+        
+f2=fruits2("apple","red","20")
+f2.display()
+
+f3=fruits3("banana","yellow","30","allday")
+f3.display()
+
+
+
+
+
